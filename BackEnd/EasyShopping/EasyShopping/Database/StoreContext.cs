8c1475e255b4c1cce851ac78164c4b1b9afdb9bb
unknown <a.r.milon927@gmail.com>
2022-06-24 23:13:03 +0600

    create api, config and add database with ef migration

---
+++ BackEnd/EasyShopping/EasyShopping/Database/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using EasyShopping.Model.Product;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyShopping.Database
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base (options)
+        {            
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BackEnd/EasyShopping/EasyShopping/Model/Product/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyShopping.Model.Product
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
